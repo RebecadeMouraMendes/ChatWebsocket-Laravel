085963ed1ac33d66906f916a75946d2ad3a506bb
BeccaDev <130665887+RebecadeMouraMendes@users.noreply.github.com>
2025-10-19 19:32:13 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
+<html>
 <head>
   <title>Pusher Test</title>
-  <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
-  <script>
 
-    // Enable pusher logging - don't include this in production
+  <!-- Importa a biblioteca JavaScript do Pusher -->
+  <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
+
+  <script>
+    // Ativa logs do Pusher no console 
     Pusher.logToConsole = true;
 
+    // Cria uma instância do Pusher com sua chave pública
     var pusher = new Pusher('73102755987e76250ed9', {
-      cluster: 'us2'
+      cluster: 'us2' // Define o cluster onde seu app tá hospedado
     });
 
+    // Inscreve o cliente (navegador) no canal
     var channel = pusher.subscribe('my-channel');
+
     channel.bind('my-event', function(data) {
+      // Mostra os dados recebidos no evento em formato JSON em um alerta
       alert(JSON.stringify(data));
     });
   </script>
 </head>
+
 <body>
   <h1>Pusher Test</h1>
   <p>
@@ -24,3 +32,4 @@
     with event name <code>my-event</code>.
   </p>
 </body>
+</html>
